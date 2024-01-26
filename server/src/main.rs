77fdc9339f54bbf3cb00cdcc1ac4082f2b14cffb
Fauzan Lubis <fauzanfauh45@gmail.com>
2024-01-26 07:55:19 +0700

    chore: move rpc impl to its own module

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,43 +1,13 @@
 use std::net::ToSocketAddrs;
 
-use capnp::capability::Promise;
-use capnp_rpc::{pry, rpc_twoparty_capnp, twoparty, RpcSystem};
+use capnp_rpc::{rpc_twoparty_capnp, twoparty, RpcSystem};
 use common::event_capnp::event_interface;
 use futures::AsyncReadExt;
 
+mod rpc_impl;
 mod template_struct;
 
 const HOST: Option<&str> = option_env!("HOST");
-
-struct EventRPCImpl;
-
-impl common::event_capnp::event_interface::Server for EventRPCImpl {
-    fn trigger(
-        &mut self,
-        params: common::event_capnp::event_interface::TriggerParams,
-        _: common::event_capnp::event_interface::TriggerResults,
-    ) -> capnp::capability::Promise<(), capnp::Error> {
-        let req = pry!(pry!(params.get()).get_event());
-
-        let identifier = pry!(pry!(req.get_identifier()).to_str());
-        let timestamp = req.get_timestamp();
-        let message = pry!(req.get_message());
-
-        let message_summary = pry!(pry!(message.get_summary()).to_str());
-        let message_body = pry!(pry!(message.get_body()).to_str());
-
-        println!("---");
-        println!(
-            "Id: {}\nTimestamp: {}\nMessage:\n\tSummary: {}\n\tBody: {}",
-            identifier, timestamp, message_summary, message_body
-        );
-        println!("---");
-
-        // TODO: Handle when being called
-
-        Promise::ok(())
-    }
-}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -50,7 +20,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::task::LocalSet::new()
         .run_until(async move {
             let listener = tokio::net::TcpListener::bind(&listen_address).await?;
-            let event_rpc: event_interface::Client = capnp_rpc::new_client(EventRPCImpl);
+            let event_rpc: event_interface::Client =
+                capnp_rpc::new_client(crate::rpc_impl::event_rpc::EventRPCImpl);
 
             println!("Server now listening at {}", listen_address);
 
---
+++ server/src/rpc_impl/event_rpc.rs
@@ -0,0 +1,32 @@
+use capnp::capability::Promise;
+use capnp_rpc::pry;
+
+pub struct EventRPCImpl;
+
+impl common::event_capnp::event_interface::Server for EventRPCImpl {
+    fn trigger(
+        &mut self,
+        params: common::event_capnp::event_interface::TriggerParams,
+        _: common::event_capnp::event_interface::TriggerResults,
+    ) -> capnp::capability::Promise<(), capnp::Error> {
+        let req = pry!(pry!(params.get()).get_event());
+
+        let identifier = pry!(pry!(req.get_identifier()).to_str());
+        let timestamp = req.get_timestamp();
+        let message = pry!(req.get_message());
+
+        let message_summary = pry!(pry!(message.get_summary()).to_str());
+        let message_body = pry!(pry!(message.get_body()).to_str());
+
+        println!("---");
+        println!(
+            "Id: {}\nTimestamp: {}\nMessage:\n\tSummary: {}\n\tBody: {}",
+            identifier, timestamp, message_summary, message_body
+        );
+        println!("---");
+
+        // TODO: Handle when being called
+
+        Promise::ok(())
+    }
+}
