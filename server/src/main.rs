e59abe0ed0391b5c6c7a2aa8fe43cc225cb0c8d1
Fauzan Lubis <fauzanfauh45@gmail.com>
2024-01-31 20:40:50 +0700

    fix: run both the rpc and http server at the same time

--- server/src/main.rs
+++ server/src/main.rs
@@ -15,12 +15,6 @@ const HTTP_HOST: Option<&str> = option_env!("HTTP_HOST");
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let rpc_listen_address = RPC_HOST
-        .unwrap_or(&"[::]:6969")
-        .to_socket_addrs()?
-        .next()
-        .expect("RPC_HOST should be a valid address");
-
     let http_listen_address = HTTP_HOST
         .unwrap_or(&"[::]:8080")
         .to_socket_addrs()?
@@ -28,17 +22,25 @@ async fn main() -> Result<(), std::io::Error> {
         .expect("HTTP_HOST should be a valid address");
 
     // Not working! I think I might need to make it run boh on different thread?
-    let _ = HttpServer::new(|| App::new().service(home))
+    let http_server = HttpServer::new(|| App::new().service(home))
         .bind(http_listen_address)?
         .run();
 
+    tokio::spawn(http_server);
+
     tokio::task::LocalSet::new()
         .run_until(async move {
+            let rpc_listen_address = RPC_HOST
+                .unwrap_or(&"[::]:6969")
+                .to_socket_addrs()?
+                .next()
+                .expect("RPC_HOST should be a valid address");
+
             let listener = tokio::net::TcpListener::bind(&rpc_listen_address).await?;
             let event_rpc: event_interface::Client =
                 capnp_rpc::new_client(crate::rpc_impl::event_rpc::EventRPCImpl);
 
-            println!("Server now listening at {}", rpc_listen_address);
+            println!("RPC server now listening at {}", rpc_listen_address);
 
             loop {
                 let (stream, _) = listener.accept().await?;
