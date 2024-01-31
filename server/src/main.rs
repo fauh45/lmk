dca312d422dccf33778cb2a7ddf8173600c9f69f
Fauzan Lubis <fauzanfauh45@gmail.com>
2024-01-31 21:46:02 +0700

    feat: finally working SIGINT listener

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,12 @@
-use std::net::ToSocketAddrs;
+use std::{net::ToSocketAddrs, process::exit};
+use tokio::signal::ctrl_c;
 
 use actix_web::{App, HttpServer};
 use capnp_rpc::{rpc_twoparty_capnp, twoparty, RpcSystem};
 use common::event_capnp::event_interface;
 use futures::AsyncReadExt;
 use http_handler::home::home;
+use tokio_util::sync::CancellationToken;
 
 mod http_handler;
 mod rpc_impl;
@@ -15,50 +17,86 @@ const HTTP_HOST: Option<&str> = option_env!("HTTP_HOST");
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Create an Arc to share shutdown signals between tasks
+    let shutdown_signal = CancellationToken::new();
+    let listen_shutdown_signal = shutdown_signal.clone();
+
+    // Spawn a task to listen for SIGINT signals and notify on shutdown
+    tokio::spawn(async move {
+        match ctrl_c().await {
+            Ok(()) => {
+                shutdown_signal.cancel();
+            }
+            Err(err) => {
+                eprintln!("Unable to listen for shutdown signal: {}", err);
+
+                exit(1);
+            }
+        }
+
+        Ok::<(), std::io::Error>(())
+    });
+
     let http_listen_address = HTTP_HOST
         .unwrap_or(&"[::]:8080")
         .to_socket_addrs()?
         .next()
         .expect("HTTP_HOST should be a valid address");
 
-    // Not working! I think I might need to make it run boh on different thread?
     let http_server = HttpServer::new(|| App::new().service(home))
         .bind(http_listen_address)?
         .run();
 
+    // Spawn HTTP server on the default runtime
     tokio::spawn(http_server);
+
+    let rpc_listen_address = RPC_HOST
+        .unwrap_or(&"[::]:6969")
+        .to_socket_addrs()?
+        .next()
+        .expect("RPC_HOST should be a valid address");
+
+    let listener = tokio::net::TcpListener::bind(&rpc_listen_address).await?;
+    let event_rpc: event_interface::Client =
+        capnp_rpc::new_client(crate::rpc_impl::event_rpc::EventRPCImpl);
+
+    println!("RPC server now listening at {}", rpc_listen_address);
 
     tokio::task::LocalSet::new()
         .run_until(async move {
-            let rpc_listen_address = RPC_HOST
-                .unwrap_or(&"[::]:6969")
-                .to_socket_addrs()?
-                .next()
-                .expect("RPC_HOST should be a valid address");
-
-            let listener = tokio::net::TcpListener::bind(&rpc_listen_address).await?;
-            let event_rpc: event_interface::Client =
-                capnp_rpc::new_client(crate::rpc_impl::event_rpc::EventRPCImpl);
-
-            println!("RPC server now listening at {}", rpc_listen_address);
-
             loop {
-                let (stream, _) = listener.accept().await?;
-                let _ = stream.nodelay()?;
+                tokio::select! {
+                    _ = listen_shutdown_signal.cancelled() => {
+                        break;
+                    }
+                    listen_result = listener.accept() => {
+                        match listen_result {
+                            Ok((stream, _)) => {
+                                let (reader, writer) = tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
+                                let network = twoparty::VatNetwork::new(
+                                    reader,
+                                    writer,
+                                    rpc_twoparty_capnp::Side::Server,
+                                    Default::default(),
+                                );
 
-                let (reader, writer) =
-                    tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
-                let network = twoparty::VatNetwork::new(
-                    reader,
-                    writer,
-                    rpc_twoparty_capnp::Side::Server,
-                    Default::default(),
-                );
+                                let rpc_system = RpcSystem::new(Box::new(network), Some(event_rpc.clone().client));
 
-                let rpc_system = RpcSystem::new(Box::new(network), Some(event_rpc.clone().client));
+                                tokio::task::spawn_local(rpc_system);
+                            }
+                            Err(err) => {
+                                eprintln!("Error while opening listen on RPC server: {}", err);
 
-                tokio::task::spawn_local(rpc_system);
+                                exit(1);
+                            }
+                        }
+                    }
+                }
             }
         })
-        .await
+        .await;
+
+    println!("Shutting down gracefully...");
+
+    Ok(())
 }
